96c8848573f9e9454b38c6e05c5e1ed3c37be177
josue <josuenavarrosan@hotmail.com>
2026-03-28 09:04:06 -0600

    fix(generator): prevent syntax errors by deleting trailing semicolons from uninitialized properties

--- src/SpToolkit.Generator/Templates/TemplateRenderer.cs
+++ src/SpToolkit.Generator/Templates/TemplateRenderer.cs
@@ -27,8 +27,6 @@ public sealed class TemplateRenderer
             sb.Append($"    public {p.ClrTypeName} {p.PropertyName} {{ get; set; }}");
             if (p.DefaultValueExpression is not null)
                 sb.Append($" = {p.DefaultValueExpression};");
-            //else
-            //    sb.Append(';');
             sb.AppendLine();
             sb.AppendLine();
         }
@@ -60,8 +58,6 @@ public sealed class TemplateRenderer
             sb.Append($"    public {p.ClrTypeName} {p.PropertyName} {{ get; set; }}");
             if (p.DefaultValueExpression is not null)
                 sb.Append($" = {p.DefaultValueExpression};");
-            //else
-            //    sb.Append(';');
             sb.AppendLine();
             sb.AppendLine();
         }
@@ -91,8 +87,6 @@ public sealed class TemplateRenderer
             sb.Append($"    public {col.ClrTypeName} {col.PropertyName} {{ get; set; }}");
             if (col.DefaultValueExpression is not null)
                 sb.Append($" = {col.DefaultValueExpression};");
-            //else
-            //    sb.Append(';');
             sb.AppendLine();
             sb.AppendLine();
         }
